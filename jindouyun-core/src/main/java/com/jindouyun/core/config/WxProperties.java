88f6bb3c057ea0555cb3f22aa3869882ef53617d
ZSZ2018211261 <2955324023@qq.com>
2020-07-22 15:32:59 +0800

    jindouyun-core修改

--- jindouyun-core/src/main/java/com/jindouyun/core/config/WxProperties.java
+++ jindouyun-core/src/main/java/com/jindouyun/core/config/WxProperties.java
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "litemall.wx")
+@ConfigurationProperties(prefix = "jindouyun.wx")
 public class WxProperties {
 
     private String appId;
---
+++ jindouyun-db/src/main/java/com/jindouyun/db/service/JindouyunStorageService.java
@@ -0,0 +1,68 @@
+package com.jindouyun.db.service;
+
+import com.github.pagehelper.PageHelper;
+import com.jindouyun.db.dao.JindouyunStorageMapper;
+import com.jindouyun.db.domain.JindouyunStorage;
+import com.jindouyun.db.domain.JindouyunStorageExample;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class JindouyunStorageService {
+
+    @Resource
+    private JindouyunStorageMapper storageMapper;
+
+    public void deleteByKey(String key) {
+        JindouyunStorageExample example = new JindouyunStorageExample();
+        example.or().andKeyEqualTo(key);
+        storageMapper.logicalDeleteByExample(example);
+    }
+
+    public void add(JindouyunStorage storageInfo) {
+        storageInfo.setAddTime(new Date(System.currentTimeMillis()));
+        storageInfo.setUpdateTime(new Date(System.currentTimeMillis()));
+        storageMapper.insertSelective(storageInfo);
+    }
+
+    public JindouyunStorage findByKey(String key) {
+        JindouyunStorageExample example = new JindouyunStorageExample();
+        example.or().andKeyEqualTo(key).andDeletedEqualTo(false);
+        return storageMapper.selectOneByExample(example);
+    }
+
+    public int update(JindouyunStorage storageInfo) {
+        storageInfo.setUpdateTime(new Date(System.currentTimeMillis()));
+        return storageMapper.updateByPrimaryKeySelective(storageInfo);
+    }
+
+    public JindouyunStorage findById(Integer id) {
+        return storageMapper.selectByPrimaryKey(id);
+    }
+
+    public List<JindouyunStorage> querySelective(String key, String name, Integer page, Integer limit, String sort, String order) {
+        JindouyunStorageExample example = new JindouyunStorageExample();
+        JindouyunStorageExample.Criteria criteria = example.createCriteria();
+
+        if (!StringUtils.isEmpty(key)) {
+            criteria.andKeyEqualTo(key);
+        }
+        if (!StringUtils.isEmpty(name)) {
+            criteria.andNameLike("%" + name + "%");
+        }
+        criteria.andDeletedEqualTo(false);
+
+        if (!StringUtils.isEmpty(sort) && !StringUtils.isEmpty(order)) {
+            example.setOrderByClause(sort + " " + order);
+        }
+
+        PageHelper.startPage(page, limit);
+        return storageMapper.selectByExample(example);
+    }
+}
